dcd79b7da86e3fbbdb2f0953cfefde69b4b0b6d6
代码白痴 <babyzhen1206@163.com>
2021-08-15 12:18:17 +0800

    Add files via upload

---
+++ hhh.java
@@ -0,0 +1,26 @@
+package com.yf.es;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+/**
+ * @PROJECT_NAME: es-springdata
+ * @DESCRIPTION:
+ * @USER:
+ * @DATE: 2021/8/15 上午10:24
+ */
+public class hhh {
+    public static void main(String[] args) {
+
+        System.out.println(8888);
+        System.out.println(9999);
+        System.out.println(6666);
+
+        System.out.println(1111);
+
+        System.out.println(6666);
+        System.out.println(6666);
+        System.out.println(0000);
+
+    }
+
+}
